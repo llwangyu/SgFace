50c143101b619970b5355a9567e1992eb3a52fc1
llwangyu <37074150@qq.com>
2018-05-31 15:07:00 +0800

    dsfafdsfdfdsafd

--- app/src/main/java/com/android/fisher/sgface/model/TestGitClass.java
+++ app/src/main/java/com/android/fisher/sgface/model/TestGitClass.java
@@ -1,7 +1,7 @@
 package com.android.fisher.sgface.model;
 
 /**
- * Created by Fisher on 2018-05-31.   test
+ * Created by Fisher on 2018-05-31.   testdsafdsafdsfdsfsdafdsfsfad
  */
 
 public class TestGitClass {
